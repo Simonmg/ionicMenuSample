afdb5f1cc9b83e8a1d6d598866a9d1c36dcc63b5
unknown <simonmg>
2020-09-14 15:27:36 -0400

    primera version

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee, faComment, faShareAlt, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  faCoffee = faCoffee;
+  faComment = faComment;
+  faShareAlt = faShareAlt;
+  faTrash = faTrash;
 
   constructor() {}
 
+  status = true;
+  clickEvent(){
+    this.status = !this.status;
+  }
 }
